07507dbe88b124ee5498d265131cfa6599e27349
Olga1507 <zheltova95@bk.ru>
2021-10-02 19:36:30 +0300

    Optimize the code.

--- SimpleNumbers.py
+++ SimpleNumbers.py
@@ -1,22 +1,9 @@
-TestArr = range(2, 101)
-
-# массив нечетных чисел
-OddIntNum = []
-# массив простых чисел
-SimpleNum = []
-
-for i in range(len(TestArr)):
-    if TestArr[i] == 2:
-        OddIntNum.append(TestArr[i])
-    else:
-        if TestArr[i] % 2 != 0:
-            OddIntNum.append(TestArr[i])
+TestArr = range(2, 1002)
 
 
 # функция проверки числа на простоту
 def is_simple_number(n):
     flag = True
-
     for i in range(2, n):
         if n % i == 0:
             flag = False
@@ -24,10 +11,7 @@ def is_simple_number(n):
     return flag
 
 
-# print(is_simple_number(5))
-
-# сохранили флаги проверки на простоту в массив простых чисел
-for i in range(len(OddIntNum)):
-    SimpleNum.append(is_simple_number(OddIntNum[i]))
-    if SimpleNum[i]:
-        print(OddIntNum[i])
+# вывод простых чисел
+for i in range(len(TestArr)):
+    if is_simple_number(TestArr[i]):
+        print(TestArr[i])
